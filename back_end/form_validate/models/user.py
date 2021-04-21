3884815c8bc1ccc725e2e46de088a012403c4bc7
RTBlanco <ronnytoribio1@hotmail.com>
2021-04-21 00:46:31 -0400

    added fetch

--- back_end/form_validate/models/user.py
+++ back_end/form_validate/models/user.py
@@ -9,3 +9,8 @@ class User(db.Model):
   id = db.Column(db.Integer, primary_key=True)
   username = db.Column(db.String(80), unique=True, nullable=False)
   password = db.Column(db.String(120), unique=False, nullable=False)
+
+  @classmethod
+  def find_by_username(self, username):
+    """ This locates the user by usernamee """
+    return User.query.filter(User.username == username).first()
--- back_end/form_validate/routes.py
+++ back_end/form_validate/routes.py
@@ -1,5 +1,5 @@
 from form_validate import app, db
-from flask import session, request, jsonify
+from flask import json, session, request, jsonify, Response
 from form_validate.models.user import User
 # from flask_login import current_user, login_required, logout_user
 
@@ -10,21 +10,29 @@ def index():
   print(request.json)
   print(dict(request.json)["username"])
   username = dict(request.json)["username"]
-  user = User.query.filter(User.username == username).first()
+  # user = User.query.filter(User.username == username).first()
+  user = User.find_by_username(username)
   print(user == None)
   if user == None:
-    return {"error": "could not find user"}
+    # return jsonify({"error": "could not find user"}), 401
+    # return Response({"error": "could not find user"}, status=401, mimetype='application/json')#jsonify({"error": "could not find user"}), 401
   else:
-    return {
-      "did_it_work": "yes"
-    }
-
+    return jsonify(id=user.id, username=user.username, password=user.password)
 
 
 @app.route('/login', methods=["POST", "GET"])
 def login():
   if request.method == 'POST':
-    session['username'] = request.form['username']
+    username = dict(request.json)["username"]
+    password = dict(request.json)["password"]
+
+    user = User.find_by_username(username)
+    if user != None and user.password == password:
+      session['username'] = username
+      return jsonify(id=user.id, username=user.username)
+    else:
+      return jsonify({"error": "incorrect login"}), 401
+
 
 
 @app.route('/logout')
