400d5e02fdadf9ae5a6df0c8d5ec63fc6cdaddaf
RTBlanco <ronnytoribio1@hotmail.com>
2021-04-23 13:15:59 -0400

    added user data to local storage

---
+++ front_end/login.js
@@ -0,0 +1,64 @@
+document.addEventListener("DOMContentLoaded", ()=>{
+  const loginForm = document.getElementById("login");
+  const username = document.getElementById("username")
+  const password = document.getElementById("password")
+
+  loginForm.addEventListener("submit", (e)=>{
+    e.preventDefault();
+    // validLogin(username, password);
+    look(username.value, password.value);
+  })
+})
+
+// function validLogin(username, password) {
+//   const formDiv = document.getElementById("form")
+//   if (username.value === "ronny" && password.value === "testing"){
+//     window.alert("you are logged in !")
+//     window.location.reload();
+//   } else {
+//     username.value = ''
+//     password.value = ''
+//     username.style.border = "1px solid red"
+//     password.style.border = "1px solid red"
+//     formDiv.style.animation = 'shake 0.3s';
+//     setTimeout(()=> formDiv.style.animation = 'none',300)
+//   }
+// }
+
+function validate(obj) {
+  const formDiv = document.getElementById("form")
+  if (obj.status === 200){
+    // window.alert("you are logged in !")
+    // window.location.reload();
+    obj.json().then( obj => {
+      const {id, username} = obj
+      localStorage.setItem('id',id)
+      localStorage.setItem('username', username)
+    })
+    // console.log(id)
+    location.href = "./index.html"
+  } else {
+    username.value = ''
+    password.value = ''
+    username.style.border = "1px solid red"
+    password.style.border = "1px solid red"
+    formDiv.style.animation = 'shake 0.3s';
+    setTimeout(()=> formDiv.style.animation = 'none',300)
+  }
+}
+
+function look(username, password) {
+  return fetch('http://localhost:5000/login',{
+    method: "POST",
+    mode: "cors",
+    headers: {
+      "content-type" : "application/json",
+      "Accept" : "application/json"
+    },
+    body: JSON.stringify({
+      username,
+      password
+    })
+  })
+  .then(resp => validate(resp))
+}
