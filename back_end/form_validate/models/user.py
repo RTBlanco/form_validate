ba96449d95385f466070edb3a71e95a4eb35bc76
RTBlanco <ronnytoribio1@hotmail.com>
2021-04-24 21:12:27 -0400

    removed comments

--- back_end/form_validate/models/user.py
+++ back_end/form_validate/models/user.py
@@ -1,9 +1,4 @@
 from form_validate import db
-# from flask_login import UserMixin
-
-# @login_manager.user_loader
-# def load_user(user_id):
-#   return User.query.get(int(user_id))
 
 class User(db.Model):
   id = db.Column(db.Integer, primary_key=True)
