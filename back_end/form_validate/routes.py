3df0dec74e1203015535bf9dd10e4c0b09e530be
RTBlanco <ronnytoribio1@hotmail.com>
2021-04-24 23:40:34 -0400

    removed uneeded code

--- back_end/form_validate/routes.py
+++ back_end/form_validate/routes.py
@@ -4,25 +4,6 @@ from form_validate.models.user import User
 from flask_cors import cross_origin
 # from flask_login import current_user, login_required, logout_user
 
-
-@app.route('/', methods=["POST", "GET"])
-@cross_origin()
-def index():
-  print('test')
-  print(request.json)
-  print(dict(request.json)["username"])
-  print(dict(request.json)["password"])
-  username = dict(request.json)["username"]
-  user = User.find_by_username(username)
-  print(user)
-  print(user == None)
-  if user != None and user.password == request.json["password"]:
-    return jsonify(id=user.id, username=user.username, password=user.password)
-  else:
-    return jsonify({"error": "could not find user"}), 422
-    # return Response({"error": "could not find user"}, status=401, mimetype='application/json')#jsonify({"error": "could not find user"}), 401
-
-
 @app.route('/login', methods=["POST", "GET"])
 def login(): 
   if request.method == 'POST':
@@ -51,8 +32,3 @@ def new():
     except AssertionError as error:
       return jsonify(error=f'{error}'), 400
 
-
-@app.route('/logout')
-def logout():
-  # remove the username from the session if it's there
-  session.pop('username', None)
