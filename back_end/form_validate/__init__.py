b840ef5f8442cdde843df36001493a0479e25730
RTBlanco <ronnytoribio1@hotmail.com>
2021-04-25 22:08:35 -0400

    initialized marshmello to flask

--- back_end/form_validate/__init__.py
+++ back_end/form_validate/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
+from flask_marshmallow import Marshmallow
 
 # from flask_login import LoginManager
 # from flask_bcrypt import Bcrypt
@@ -10,6 +11,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db/test.db'
 app.secret_key = "testing"
 CORS(app)
 db = SQLAlchemy(app)
+ma = Marshmallow(app)
 # login_manager = LoginManager()
 # login_manager.init_app(app)
 
